c7dcffa03d9e29b90ff44ff79019f3a5df520588
jxw12138 <130843852+jxw12138@users.noreply.github.com>
2023-05-23 23:33:36 +0800

    Update xunlei_setup.sh

--- xunlei_setup.sh
+++ xunlei_setup.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 mkdir -p /opt/downloads
-
 docker run -d --name=xunlei --hostname=myxunlei --net=bridge -p 2345:2345 -v /root/xunlei:/xunlei/data -v /opt/downloads:/xunlei/downloads --restart=unless-stopped --privileged cnk3x/xunlei:latest
-
-
