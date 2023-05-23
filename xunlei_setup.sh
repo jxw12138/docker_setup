a78052f53aaafdfa4c05d6796de92685a3a9216c
jxw12138 <130843852+jxw12138@users.noreply.github.com>
2023-05-23 23:39:45 +0800

    Add files via upload

--- xunlei_setup.sh
+++ xunlei_setup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 mkdir -p /opt/downloads
+
 docker run -d --name=xunlei --hostname=myxunlei --net=bridge -p 2345:2345 -v /root/xunlei:/xunlei/data -v /opt/downloads:/xunlei/downloads --restart=unless-stopped --privileged cnk3x/xunlei:latest
